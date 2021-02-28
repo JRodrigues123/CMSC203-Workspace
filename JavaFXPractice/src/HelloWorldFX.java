7c40c47d95a1d1c066f149ed8726e1b6af1474e0
Justin Rodrigues <justinrodrigues@jasmines-air.home>
2021-02-28 15:22:22 -0500

    fixed

--- JavaFXPractice/src/HelloWorldFX.java
+++ JavaFXPractice/src/HelloWorldFX.java
@@ -1,32 +1,15 @@
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
-public class HelloWorldFX extends Application {
+public class HelloWorldFX {
     public static void main(String[] args) {
-        launch(args);
+        
+    	/*
+    	 * For a quicker way to get a system.out.print statement
+    	 * type "sysout" then control+space on your keyboard
+    	 */
+    	
+    	System.out.println();
+    	
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+   
     }
-}
