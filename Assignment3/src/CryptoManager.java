e7705f6c15280e771aecdea45c97be797f77cd7f
Justin Rodrigues <justinrodrigues@jasmines-air.home>
2021-03-05 13:48:42 -0500

    I did it
    Signed-off-by: Justin Rodrigues <justinrodrigues@jasmines-air.home>

--- Assignment3/src/CryptoManager.java
+++ Assignment3/src/CryptoManager.java
@@ -13,7 +13,31 @@ public class CryptoManager {
 	 * @return true if all characters are within the allowable bounds, false if any character is outside
 	 */
 	public static boolean stringInBounds (String plainText) {
-		throw new RuntimeException("method not implemented");
+		
+		//This will the boolean that we'll return
+		boolean isValid = true;
+		
+		/*
+		 * This loop will check  if the variable is outside the upper and lower bound
+		 */
+		for(int i = 0; i < plainText.length(); i++) {
+			
+			//This variable will hold a character from the array
+			char a = plainText.charAt(i);
+			
+			/*
+			 * This will check whether or not the character
+			 * is within bounds
+			 */
+			if(a < LOWER_BOUND || a > UPPER_BOUND) {
+				
+				isValid = false;
+			}
+			
+		}
+		
+		//Returns the boolean
+		return isValid;
 	}
 
 	/**
@@ -24,7 +48,45 @@ public class CryptoManager {
 	 * @return the encrypted string
 	 */
 	public static String encryptCaesar(String plainText, int key) {
-		throw new RuntimeException("method not implemented");
+		
+		/*
+		 * We first need to make sure that we 
+		 * checked the logic for plainText and see
+		 * if it's true
+		 */
+		if(!stringInBounds(plainText)) {
+			
+			return " ";
+		}
+		
+		/*
+		 * This will be used to encrypt the Ceasar
+		 */
+		String encrypted = " ";
+		
+		//This will go through the whole String of chars
+		for(int i = 0; i < plainText.length(); i++){
+			
+			//This is to pull a character from the string variable
+			char c = plainText.charAt(i);
+			//Changes the char "c" to an int so it's comparable
+			int encrypt = (int)c + key;
+			
+			/*
+			 * This while method is used to make the encrypt
+			 * variable lower than the upper bound
+			 */
+			while(encrypt > UPPER_BOUND) {
+				
+				encrypt -= RANGE;
+				
+			}
+			
+			//This'll append to the String
+			encrypted += (char)encrypt;
+			
+		}
+		return encrypted;
 	}
 	
 	/**
@@ -36,7 +98,48 @@ public class CryptoManager {
 	 * @return the encrypted string
 	 */
 	public static String encryptBellaso(String plainText, String bellasoStr) {
-		throw new RuntimeException("method not implemented");
+			
+		/*
+		 * @param encrypted, used to return a string after
+		 * it gets encrypted
+		 * @param uBound, is used to help make the certain char be 
+		 * under a certain amount
+		 * @param bellaso, this is used to get the length 
+		 * of the bellaso string
+		 */
+		String encrypted = " ";
+		int uBound = (int)UPPER_BOUND;
+		int bellaso = bellasoStr.length();
+		
+		//A for loop to get through the bellaso String
+		for(int i = 0; i < plainText.length(); i++){
+			
+			/*
+			 * This holds a character from the String
+			 */
+			char c =  plainText.charAt(i);
+			
+			/*
+			 * This is used to get the total for the bellaso
+			 * String while changing it to an int
+			 */
+			int enc = (int)c + (int)bellasoStr.charAt(i % bellaso);
+			
+			/*
+			 * This will help make the enc
+			 * variable in bound
+			 */
+			while(enc > uBound) {
+				
+				enc -= RANGE;
+			}
+			
+			//This'll append to the encrypted
+			encrypted += (char)enc;
+		}
+		
+		//Returns the string 
+		return encrypted;
 	}
 	
 	/**
@@ -48,7 +151,37 @@ public class CryptoManager {
 	 * @return the plain text string
 	 */
 	public static String decryptCaesar(String encryptedText, int key) {
-		throw new RuntimeException("method not implemented");
+		
+		/*
+		 * @param decrypted, this is what the method will return
+		 */
+		String decrypted = " ";
+		
+		/*
+		 * This loop will check every single character in the
+		 * encrypted String
+		 */
+		for(int a = 0; a < encryptedText.length(); a++) {
+			
+			//This will hold the current character from the  string
+			char c = encryptedText.charAt(a);
+			//This will be used for decryption
+			int decrypt = (int)c - key;
+			
+			/*
+			 * This while loop will check to make sure that the 
+			 * certain char variable stays in bounds
+			 */
+			while(decrypt < LOWER_BOUND) {
+				
+				decrypt += RANGE;
+			}
+			
+			//Appends to the decrypted String
+			decrypted += (char)decrypt;
+		}
+		
+		return decrypted;
 	}
 	
 	/**
@@ -60,6 +193,39 @@ public class CryptoManager {
 	 * @return the decrypted string
 	 */
 	public static String decryptBellaso(String encryptedText, String bellasoStr) {
-		throw new RuntimeException("method not implemented");
+		
+		//This is usd to get the length of the bellaso String
+		int bLength = bellasoStr.length();
+		//This is to get the lower bound in terms of int
+		int lowBound = (int)LOWER_BOUND;
+		//This is what will return frmo this method
+		String decrypted = " ";
+		
+		/*
+		 * This for loop will help encrypt the bellaso String to 
+		 * get in in bounds
+		 */
+		for(int z = 0; z < encryptedText.length(); z++) {
+			
+			
+			//This holds a character
+			char f = encryptedText.charAt(z);
+			//this decrypts a character from the bellaso variables
+			int decryptBellaso = (int)f - (int)bellasoStr.charAt(z % bLength);
+			
+			/*
+			 * A while loop to make sure that the variable
+			 */
+			while(decryptBellaso < lowBound){
+				
+				decryptBellaso += RANGE;
+			}
+			
+			//It adds the chars to be decrypted
+			decrypted += (char)decryptBellaso;
+			
+		}
+		
+		return decrypted;
 	}
 }
