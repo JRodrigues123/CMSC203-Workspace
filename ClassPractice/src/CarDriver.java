850d8f632c69d3cd558f968b2f189f20699a1094
Justin Rodrigues <justinrodrigues@jasmines-air.home>
2021-03-12 14:22:52 -0500

    hello
    Signed-off-by: Justin Rodrigues <justinrodrigues@jasmines-air.home>

---
+++ ClassPractice/src/CarDriver.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class CarDriver {
+
+	public static void main(String[] args) {
+		
+		Car c1 = new Car("Jaguar", "2018");
+		Car c2 = new Car("Honda Civic", "2020");
+		
+		ArrayList<Car> cars = new ArrayList<Car>();
+		
+		cars.add(c1);
+		cars.add(c2);
+		
+		System.out.println(cars.get(0).getName() + " and " + cars.get(1).getName());
+		
+	
+		
+		
+		
+	}
+
+}
