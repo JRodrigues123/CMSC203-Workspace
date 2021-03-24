55472163667abc24b7077fba18c3ed9400ad01c2
Justin Rodrigues <justinrodrigues@jasmines-air.home>
2021-03-24 11:26:25 -0400

    Yess
    Signed-off-by: Justin Rodrigues <justinrodrigues@jasmines-air.home>

---
+++ Practice2/src/Animal.java
@@ -0,0 +1,58 @@
+//Data Element
+
+public class Animal {
+
+	private int age;
+	private String size;
+	private double weight;
+	
+	private String name;
+
+	public Animal(int age, String size, double weight, String name) {
+		super();
+		this.age = age;
+		this.size = size;
+		this.weight = weight;
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ Practice2/src/AnimalDriver.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+import java.util.Scanner;
+
+//Client Class/Driver Class
+
+public class AnimalDriver {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		
+				
+		
+		
+	}
+
+}
