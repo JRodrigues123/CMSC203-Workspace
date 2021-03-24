a8a29193a3bd15bf6756d0aa4dec9d450520497d
Justin Rodrigues <justinrodrigues@jasmines-air.home>
2021-03-24 11:46:46 -0400

    fixed some thing

--- Practice2/src/Animal.java
+++ Practice2/src/Animal.java
@@ -7,7 +7,33 @@ public class Animal {
 	private double weight;
 	
 	private String name;
-
+	
+	//Dog subclass
+	public class Dog{
+		
+			private String breed;
+			private String furColor;
+			
+			
+			public String getBreed() {
+				return breed;
+			}
+			public void setBreed(String breed) {
+				this.breed = breed;
+			}
+			public String getFurColor() {
+				return furColor;
+			}
+			public void setFurColor(String furColor) {
+				this.furColor = furColor;
+			}
+			
+			
+		
+		
+	}
+	
+	
 	public Animal(int age, String size, double weight, String name) {
 		super();
 		this.age = age;
@@ -15,6 +41,7 @@ public class Animal {
 		this.weight = weight;
 		this.name = name;
 	}
+	
 
 	public int getAge() {
 		return age;
--- Practice2/src/Mammal.java
+++ Practice2/src/Mammal.java
@@ -1,4 +1,13 @@
 
-public class Mammal {
+public class Mammal extends Animal {
 
+	private boolean warmBlooded;
+	
+	
+	
+	public Mammal(int age, String size, double weight, String name) {
+		super(age, size, weight, name);
+		// TODO Auto-generated constructor stub
+	}
+		
 }
