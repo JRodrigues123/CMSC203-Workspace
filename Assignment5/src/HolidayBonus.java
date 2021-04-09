e03323ccfd1e1b5490f2e15a1beab3e707df11cb
Justin Rodrigues <justinrodrigues@jasmines-air.home>
2021-04-09 15:32:52 -0400

    The start of assigment 5
    Signed-off-by: Justin Rodrigues <justinrodrigues@jasmines-air.home>

---
+++ Assignment5/src/HolidayBonus.java
@@ -0,0 +1,42 @@
+
+public class HolidayBonus {
+	
+	/**
+	 * Empty Constructor
+	 */
+	public HolidayBonus() {
+		
+		
+	}
+	
+	/**
+	 * This calculates the holiday bonus
+	 * @param data
+	 * @param high
+	 * @param low
+	 * @param other
+	 * @return
+	 */
+	static double[] calculateHolidayBonus(double[][] data, double high, double low, double other) {
+		
+		
+	}
+	
+	/**
+	 * This will calculate the total holiday bonus
+	 * @param data
+	 * @param high
+	 * @param low
+	 * @param other
+	 * @return
+	 */
+	static double calculateTotalHolidayBonus(double[][] data, double high, double low, double other ) {
+		
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ Assignment5/src/TwoDimRaggedArrayUtility.java
@@ -0,0 +1,121 @@
+import java.io.File;
+public final class TwoDimRaggedArrayUtility {
+	
+	/**
+	 * An empty constructor
+	 */
+	public TwoDimRaggedArrayUtility() {
+		
+	}
+	
+	/**
+	 * This returns the average from the data array
+	 * @param data
+	 * @return
+	 */
+	static double getAverage(double[][] data) {
+		
+		return 0.0;
+	}
+	
+	/**
+	 * This method will get the column total
+	 * @param data
+	 * @param col
+	 * @return
+	 */
+	static double getColumnTotal(double[][] data, int col) {
+		
+		return 0;
+	}
+	
+	/**
+	 * This will yeild the highest array in the data
+	 * @param data
+	 * @return
+	 */
+	static double getHighestInArray(double[][] data) {
+		
+	}
+	
+	/**
+	 * This will retur the greates number in the 
+	 * columns
+	 * @param data
+	 * @param col
+	 */
+	static double getHighestInColumn(double[][] data, int col) {
+		
+	}
+	
+	
+	static int getHighestInColumnIndex(double[][] data, int col) {
+		
+		
+	}
+	
+	
+	static double getHighestInRow(double[][] data, int row) {
+		
+		
+	}
+	
+	
+	static int getHighestInRowIndex(double[][] data, int row) {
+		
+		
+	}
+	
+	
+	static double getLowestInArray(double[][] data) {
+		
+		
+	}
+	
+	
+	static double getLowestInColumn(double[][] data, int col) {
+		
+		
+	}
+	
+	
+	static int getLowestInColumnIndex(double[][] data, int col) {
+		
+		
+	}
+	
+	
+	static double getLowestInRow(double[][] data, int row) {
+		
+		
+	}
+	
+	
+	static int getLowestInRowIndex(double[][] data, int row) {
+		
+		
+	}
+	
+	
+	static double getRowTotal(double[][] data, int row) {
+		
+		
+	}
+	
+	
+	static double getTotal(double[][] data) {
+		
+		
+	}
+	
+	
+	static double[][] readFile(File file){
+		
+		
+	}
+	
+	static void writeToFile(double[][] data, File outputFile) {
+		
+		
+	}
+}
