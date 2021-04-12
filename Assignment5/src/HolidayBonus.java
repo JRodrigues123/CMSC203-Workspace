9d1317bf7465b8e3386952f845ad73b8388cca12
Justin Rodrigues <justinrodrigues@jasmines-air.home>
2021-04-12 10:55:37 -0400

    stuff

--- Assignment5/src/HolidayBonus.java
+++ Assignment5/src/HolidayBonus.java
@@ -1,4 +1,7 @@
-
+/**
+ * 
+ * @author justinrodrigues
+ */
 public class HolidayBonus {
 	
 	/**
@@ -6,11 +9,11 @@ public class HolidayBonus {
 	 */
 	public HolidayBonus() {
 		
-		
+		//This constructor is empty
 	}
 	
 	/**
-	 * This calculates the holiday bonus
+	 * This calculates the holiday bonus for each store 
 	 * @param data
 	 * @param high
 	 * @param low
@@ -18,6 +21,58 @@ public class HolidayBonus {
 	 * @return
 	 */
 	static double[] calculateHolidayBonus(double[][] data, double high, double low, double other) {
+		
+		/*
+		 * This array is to store the holiday bonus from
+		 * each store
+		 */
+		double[] bonusArray = new double[data.length];
+		
+		/*
+		 * A nested for loop to get the rows
+		 * and columns from the array
+		 */
+		for(int r  = 0; r < data.length; r++) {
+			
+			/*
+			 * This is a loop variable that will 
+			 * hold the holiday bonus from each store
+			 */
+			double holidayBonus = 0.0;
+			
+			for(int c = 0; c < data[r].length; c++) {
+				
+				/*
+				 * 
+				 */
+				if(r == TwoDimRaggedArrayUtility.getLowestInColumnIndex(data, c)) {
+					
+					holidayBonus += low;
+				}
+				else if(r == TwoDimRaggedArrayUtility.getHighestInColumnIndex(data, c)) {
+					
+					holidayBonus += high;
+				}
+				else if(data[r][c] <= 0) {
+					
+					holidayBonus += 0.0;
+					
+				}
+				else {
+					
+					holidayBonus += other;
+					
+				}
+				
+			}
+			
+			bonusArray[r] = holidayBonus;
+			
+		}
+		
+		//
+		return bonusArray;
+		
 		
 		
 	}
@@ -32,7 +87,28 @@ public class HolidayBonus {
 	 */
 	static double calculateTotalHolidayBonus(double[][] data, double high, double low, double other ) {
 		
+		/*
+		 * This is to hold the entire total 
+		 * holiday bonus
+		 */
+		double totalBonus = 0;
 		
+		/*
+		 * This is used to get the total bonus from every store
+		 */
+		double[] totalBonusArray = HolidayBonus.calculateHolidayBonus(data, high, low, other);
+		
+		/*
+		 * An enhanced for loop
+		 */
+		for(double bonus: totalBonusArray) {
+			
+			totalBonus += bonus;
+			
+		}
+		
+		//
+		return totalBonus;
 	}
 	
 	
--- Assignment5/src/TwoDimRaggedArrayUtility.java
+++ Assignment5/src/TwoDimRaggedArrayUtility.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 public final class TwoDimRaggedArrayUtility {
 	
 	/**
@@ -6,27 +10,96 @@ public final class TwoDimRaggedArrayUtility {
 	 */
 	public TwoDimRaggedArrayUtility() {
 		
+		//EMPTY
 	}
 	
 	/**
 	 * This returns the average from the data array
 	 * @param data
-	 * @return
+	 * @return totalSum/count
 	 */
 	static double getAverage(double[][] data) {
 		
-		return 0.0;
+		
+		/*
+		 * This will hold the total sum 
+		 * from the array
+		 */
+		double totalSum = 0;
+		
+		/*
+		 * This will keep count of the amount of numbers in the array
+		 * in order to get the average
+		 */
+		double count = 0;
+		
+		/*
+		 * This for loop is to get the rows
+		 */
+		for(int a = 0; a < data.length; a++) {
+			
+			//This second for loop will grt the columns
+			for(int b = 0; b < data[a].length; b++) {
+				
+				//The numbers from data will add onto the total
+				totalSum += data[a][b];
+				
+				//This keeps count
+				count++;
+				
+				
+			}
+			
+		}
+		
+		//This will yield the average
+		return totalSum/count;
 	}
 	
 	/**
-	 * This method will get the column total
+	 * This method will get the column total from
+	 * the array
 	 * @param data
 	 * @param col
-	 * @return
+	 * @return totalCol
 	 */
 	static double getColumnTotal(double[][] data, int col) {
 		
-		return 0;
+		//This will be used to hold the total column
+		double totalCol = 0;
+		
+		/*
+		 * This for loop will be used to 
+		 * get the numbers from the data array
+		 */
+		for(int c = 0; c < data.length; c++) {
+			
+			//This will check if any number is less than or equal to 0
+			if(col < 0 || data[c].length <= col) {
+				
+				//Nothing
+			}
+			
+			/*
+			 * This will add the number from the
+			 * columns to the total columns
+			 */
+			if(col == 1 && data[c].length == 1) {
+				
+				//This adds onto the total columns variable
+				totalCol += data[c][col-1];
+			}
+			else {
+				
+				totalCol += data[c][col];
+			}
+			
+		}
+		
+		/*
+		 * This returns the total amount from t
+		 */
+		return totalCol;
 	}
 	
 	/**
@@ -36,6 +109,33 @@ public final class TwoDimRaggedArrayUtility {
 	 */
 	static double getHighestInArray(double[][] data) {
 		
+		/*
+		 * This variable will hold the highest number 
+		 * in the given array
+		 */
+		double largeNum = 0;
+		
+		/*
+		 * A nested for loop to get through each number 
+		 * in the given array
+		 */
+		for(int d = 0; d < data.length; d++) {
+			
+			for(int e = 0; e < data[d].length; e++) {
+				
+				/*
+				 * if true, it'll hold the largest 
+				 * value from the array
+				 */
+				if(data[d][e] > largeNum) {
+					
+					largeNum = data[d][e];
+				}
+				
+			}
+		}
+		
+		return largeNum;
 	}
 	
 	/**
@@ -46,22 +146,156 @@ public final class TwoDimRaggedArrayUtility {
 	 */
 	static double getHighestInColumn(double[][] data, int col) {
 		
+		/*
+		 * This variable will hold the largest 
+		 * number within the columns
+		 */
+		double largeNumCol = 0;
+		
+		/*
+		 * This will go through the array
+		 * to get the numbers from each column
+		 */
+		for(int f = 0; f < data.length; f++) {
+			
+			/*
+			 * Nothing will happen if the 
+			 * col number is less than 0 
+			 */
+			if(col < 0 || data[f].length <= col) {
+				
+				//Nothing will happen
+			}
+			if(data[f][col] > largeNumCol) {
+				
+				largeNumCol = data[f][col];
+			}
+		}
+		
+		return largeNumCol;
 	}
 	
 	
 	static int getHighestInColumnIndex(double[][] data, int col) {
 		
+		//This variable will be returned
+		int g = 0;
 		
+		//This variable will keep hold of the index
+		double highNum = 0;
+		
+		/*
+		 * A regular loop to get through the array
+		 */
+		for(int h = 0; h < data.length; h++) {
+			
+			if(col < 0 || data[h].length <= 0) {
+				
+				//Does nothing
+			}
+			if(highNum < data[h][col]){
+				
+				g = h;
+				
+				highNum = data[h][col];
+				
+			}
+			
+		}
+		
+		//Returns the highest column index
+		return g;
 	}
 	
 	
 	static double getHighestInRow(double[][] data, int row) {
 		
+		/*
+		 * This variable will be 
+		 * used to hold the highest number in 
+		 * the row
+		 */
+		double highInRow = 0;
 		
+		/*
+		 * This if statement will check
+		 * whether the row is less than 0 
+		 * and that the array length is less
+		 * or equal to the row.
+		 */
+		if(row < 0 || data.length < row) {
+			
+			return 0;
+		}
+		
+		/*
+		 * This for loop will go through rows from the array 
+		 * and find the highest number in there
+		 */
+		for(int i = 0; i < data[row].length; i++) {
+			
+			//If true, highInRow will get the highest value
+			if(highInRow < data[row][i]) {
+				
+				highInRow = data[row][i];
+			}
+			
+		}
+		
+		return highInRow;
 	}
 	
 	
 	static int getHighestInRowIndex(double[][] data, int row) {
+		
+		/*
+		 * This variable will hold the index with the 
+		 * highest number
+		 */
+		int j = 0; 
+		
+		/*
+		 * This will hold the highest row
+		 * value for comparing
+		 */
+		double highRow = 0;
+		
+		/*
+		 * If the row less than 0 or 
+		 * the length of the array iss less than or equal
+		 * to the row it...
+		 * @return 0
+		 * 
+		 */
+		if(row < 0 || row > data.length) {
+			
+			return 0;
+		}
+		
+		/*
+		 * This loop goes through the array for 
+		 * the rows
+		 */
+		for(int k = 0; k < data[row].length; k++) {
+			
+			/*
+			 * If the value in the array
+			 * is larger than highRow, it becomes the value of 
+			 * highRow
+			 */
+			if(highRow < data[row][k]) {
+				
+				//This gets the index for the highest value
+				j = k;
+				
+				highRow = data[row][k];
+				
+			}
+			
+		}
+		
+		//This returns the index with the highest value
+		return j;
 		
 		
 	}
@@ -69,53 +303,369 @@ public final class TwoDimRaggedArrayUtility {
 	
 	static double getLowestInArray(double[][] data) {
 		
+		/*
+		 * This variable will be used to
+		 * hold the lowest number from 
+		 * the array
+		 */
+		double lowNum = 0;
+		
+		/*
+		 * Another nested loop for the array 
+		 */
+		for(int l = 0; l < data.length; l++) {
+			
+			
+			for(int m = 0; m < data[l].length; m++) {
+				
+				/*
+				 * This will give the  lowNum
+				 * variable the lowest value from 
+				 * the array
+				 */
+				if(lowNum == 0 || lowNum > data[l][m]) {
+					
+					lowNum = data[l][m];
+				}
+				
+			}
+			
+		}
+		
+		//This will return the lowest value
+		return lowNum;
 		
 	}
 	
 	
 	static double getLowestInColumn(double[][] data, int col) {
 		
+		/*
+		 * This variable here is to keep 
+		 * track of the lowest number in the 
+		 * array in order for comparison
+		 */
+		double lowestNum = 0;
 		
+		/*
+		 * 
+		 */
+		for(int n = 0; n < data.length; n++) {
+			
+			
+			/*
+			 * 
+			 */
+			if(col < 0 || data[n].length <= col) {
+				
+				//This make the statement continue
+				
+			}
+			if(lowestNum == 0 || lowestNum > data[n][col]) {
+				
+				lowestNum = data[n][col];
+				
+			}
+			
+			
+		}
+		
+		//
+		return lowestNum;
 	}
 	
 	
 	static int getLowestInColumnIndex(double[][] data, int col) {
 		
+		/*
+		 * 
+		 */
+		double lowNum = 0;
 		
+		/*
+		 * 
+		 */
+		int o = 0;
+		
+		/*
+		 * 
+		 */
+		for(int p = 0; p < data.length; p++) {
+			
+			
+			/*
+			 * 
+			 */
+			if(data[p].length <= col || col < 0) {
+				
+				//This just continues 
+			}
+			if(lowNum > data[p][col] || lowNum == 0 ){
+				
+				//
+				o = p;
+				
+				//
+				lowNum = data[p][col];
+				
+			}
+		}
+		
+		//
+		return o;
 	}
 	
 	
 	static double getLowestInRow(double[][] data, int row) {
 		
 		
+		/*
+		 * this variable will hold the 
+		 * lowest number from the row in the array
+		 */
+		double rowLowNum = 0;
+		
+		/*
+		 * 
+		 */
+		if(row < 0 || data.length < row) {
+			
+			return 0;
+			
+		}
+		
+		/**
+		 *  
+		 */
+		for(int q = 0; q < data[q].length; q++) {
+			
+			
+			if(rowLowNum > data[row][q] || rowLowNum == 0) {
+				
+				//
+				rowLowNum = data[row][q];
+			}
+		}
+		
+		//
+		return rowLowNum;
+		
 	}
 	
 	
 	static int getLowestInRowIndex(double[][] data, int row) {
 		
+		/*
+		 * 
+		 */
+		double lowNum = 0;
 		
+		/*
+		 * 
+		 */
+		int r = 0;
+		
+		/*
+		 * 
+		 */
+		if(row < 0 || row > data.length) {
+			
+			return 0;
+		}
+		
+		/*
+		 * 
+		 */
+		for(int s = 0; s < data[s].length; s++) {
+			
+			/*
+			 * 
+			 */
+			if(lowNum > data[row][s] || lowNum == 0) {
+				
+				//
+				r = s;
+				
+				//
+				lowNum = data[row][s];
+				
+			}
+			
+		}
+		
+		//
+		return r;
 	}
 	
-	
+	/**
+	 * It get the total sum from the rows
+	 * @param data
+	 * @param row
+	 * @return
+	 */
 	static double getRowTotal(double[][] data, int row) {
 		
+		/*
+		 * This will be used to hold the 
+		 * sum of the rows
+		 */
+		double sumRows = 0;
+		
+		/*
+		 * This for loop will go through the rows 
+		 * and get their number
+		 */
+		for(int l =0; l < data[row].length; l++) {
+			
+			sumRows += data[row][l];
+		}
+		
+		return sumRows;
 		
 	}
 	
 	
 	static double getTotal(double[][] data) {
 		
+		/*
+		 * This variable will hold
+		 * the total sum from the array, column and rows
+		 */
+		double arrayTotal = 0;
 		
+		/*
+		 * This nested loop is here to acquire
+		 * the values from the array
+		 */
+		for(int m = 0; m < data.length; m++) {
+			
+			
+			for(int n = 0; n < data[m].length; n++) {
+				
+				//This adds onto the arrayTotal variable
+				arrayTotal += data[m][n];
+				
+			}
+		}
+		
+		return arrayTotal;
 	}
 	
 	
-	static double[][] readFile(File file){
+	static double[][] readFile(File file) throws FileNotFoundException{
 		
+		/*
+		 * This variable will keep track of 
+		 * the amount of rows in the array
+		 */
+		int rowAmount = 0;
 		
+		/*
+		 * This is used to read of the file 
+		 * from the...file
+		 */
+		Scanner input = new Scanner(file);
+		
+		/*
+		 * This is a temp array to use 
+		 * so we can get the rows
+		 */
+		String[][] tempArray = new String[10][10];
+		
+		/*
+		 * 
+		 */
+		while(input.hasNext()) {
+			
+			tempArray[rowAmount] = input.nextLine().split(" ");
+			
+			//This will keep count of the amount of rows
+			rowAmount++;
+			
+		}
+		
+		//Closes the scanner for the file
+		input.close();
+		
+		/*
+		 * This array will be returned
+		 */
+		double[][] resultArray = new double[rowAmount][];
+		
+		/*
+		 * This nested for loop goes through the array 
+		 * and puts the rows and columns
+		 * in the result array
+		 */
+		for(int r = 0; r < rowAmount; r++) {
+			
+			/*
+			 * This variable is used to hold the column index
+			 * and is the same length as the amount of rows'
+			 * from the temp array
+			 */
+			int columns = tempArray[rowAmount].length;
+			
+			resultArray[rowAmount] = new double[columns];
+			
+			//This loop is to go through the columns
+			for(int c = 0; c < columns; c++) {
+				
+				/*
+				 * This puts the value in the result array from 
+				 * the temp array
+				 */
+				resultArray[r][c] = Double.parseDouble(tempArray[r][c]);
+				
+			}
+			
+			
+		}
+		
+		//Returns the double array
+		return resultArray;
 	}
 	
-	static void writeToFile(double[][] data, File outputFile) {
+	/**
+	 * 
+	 * @param data
+	 * @param outputFile
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	static void writeToFile(double[][] data, File outputFile) throws IOException, FileNotFoundException{
 		
+		
+		/**
+		 * This will create a fileWriter object 
+		 * so you'll be able to write a file
+		 */
+		FileWriter fileOutput = new FileWriter(outputFile);
+		
+		/*
+		 * This nested loop will read the given array
+		 */
+		for(int t = 0; t < data.length; t++) {
+			
+			for(int u = 0; u < data[t].length; u++) {
+				
+				/*
+				 * This command will write onto the file 
+				 * with the values from the arrays
+				 */
+				fileOutput.write(data[t][u] + " ");
+				
+				
+			}
+			
+			//This will print to the next line
+			fileOutput.write("\n");
+		}
+		
+		/*
+		 * This will be closing down the fileOutput object
+		 */
+		fileOutput.close();
 		
 	}
 }
